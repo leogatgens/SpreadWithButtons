a03f84391c359ef1c060994340677ccf30ef029a
Leonardo Meléndez <leogatgens@gmail.com>
2018-02-21 15:27:10 -0600

    Testing

---
+++ SpreadWithButtons/CustomFilterButton.vb
@@ -0,0 +1,130 @@
+﻿
+
+<Serializable()> Public Class CustomFilterButton
+
+    'Create a sub-class inheriting BaseFilterItem class.
+    Inherits FarPoint.Win.Spread.BaseFilterItem
+
+    Dim sv As FarPoint.Win.Spread.SheetView = Nothing
+
+    Public Sub New()
+    End Sub
+
+    Public Sub New(ByVal sheetView As FarPoint.Win.Spread.SheetView)
+        Me.sv = sheetView
+    End Sub
+
+    '---------------------------------------------------------
+    'Return names to be displayed in the drop-down list.
+    '---------------------------------------------------------
+    Public Overrides ReadOnly Property DisplayName() As String
+        Get
+            Dim list As New ArrayList
+            Dim lista2 As New System.Text.StringBuilder()
+
+
+
+
+
+
+
+
+            Return "FiltrarBoton"
+
+        End Get
+    End Property
+
+    '-----------------------------------------------------------------------------------------
+    'Set sheets to filters.
+    '-----------------------------------------------------------------------------------------
+    Public Overrides WriteOnly Property SheetView() As FarPoint.Win.Spread.SheetView
+        Set(ByVal Value As FarPoint.Win.Spread.SheetView)
+            sv = Value
+        End Set
+    End Property
+
+    '-----------------------------------------------------------------------------------------
+    'Evaluate specified values by particular conditions.
+    '-----------------------------------------------------------------------------------------
+    Public Function IsFilteredIn(ByVal value As String) As Boolean
+        If (value = "Hola") Then
+            'Return True only when the following conditions are satisfied.
+            '(1)Values are entered.
+            '(2)Values are not lower than 10.
+            '(3)Values are not greater than 50.
+            Return True
+        End If
+        Return False
+    End Function
+
+    '-----------------------------------------------------------------------------------------
+    'Display names returned by DisplayName property.
+    '-----------------------------------------------------------------------------------------
+    Public Overrides Function ShowInDropDown(ByVal columnIndex As Integer, ByVal filteredInRowList() As Integer) As Boolean
+        Return True
+    End Function
+
+    '-----------------------------------------------------------------------------------------
+    'Execute filtering in the column specified in a sheet.
+    '-----------------------------------------------------------------------------------------
+    Public Overrides Function Filter(ByVal columnIndex As Integer) As Integer()
+
+        Dim list As New ArrayList
+        Dim returnList As Integer() = Nothing
+        Dim row As Integer
+
+        If (sv Is Nothing) Then
+            Return returnList
+        End If
+
+        For row = 0 To sv.RowCount - 1
+            Dim celdaBoton = DirectCast(sv.Cells(row, columnIndex).Formatter, FarPoint.Win.Spread.CellType.ButtonCellType).[Text]
+
+            'If Not (sv.GetValue(row, columnIndex) = Nothing) Then
+
+
+            Dim value As String = celdaBoton
+            If IsFilteredIn(value) Then
+                'Add row indexes that meet conditions sequentially.
+                list.Add(row)
+            End If
+
+        Next row
+
+        'When there are any rows that meet conditions, copy them to arrays.
+        If list.Count > 0 Then
+            returnList = New Integer(list.Count - 1) {}
+            list.CopyTo(returnList)
+            list.Clear()
+        End If
+
+        'Return all row indexes that meet conditions.
+        Return returnList
+
+    End Function
+
+    '-----------------------------------------------------------------------------------------
+    'When any data that has to be serialized is included in a custom filter class, override it.
+    '-----------------------------------------------------------------------------------------
+    Public Overrides Function Serialize(ByVal w As System.Xml.XmlTextWriter) As Boolean
+
+        w.WriteStartElement("CustomFilter")
+        MyBase.Serialize(w)
+        w.WriteEndElement()
+        Return True
+
+    End Function
+
+    Public Overrides Function Deserialize(ByVal r As System.Xml.XmlNodeReader) As Boolean
+
+        If r.NodeType = System.Xml.XmlNodeType.Element Then
+            If r.Name.Equals("CustomFilter") Then
+                MyBase.Deserialize(r)
+            End If
+        End If
+        Return True
+
+    End Function
+
+End Class
+
---
+++ SpreadWithButtons/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class form1
+
+
+    Dim WithEvents FpSpread1 As New FarPoint.Win.Spread.FpSpread()
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        FpSpread1 = New FarPoint.Win.Spread.FpSpread
+        Dim hoja1 As New FarPoint.Win.Spread.SheetView
+        hoja1.RowCount = 20
+        FpSpread1.Sheets.Add(hoja1)
+        FpSpread1.Dock = DockStyle.Fill
+        Me.Controls.Add(FpSpread1)
+
+    End Sub
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+
+        GenerarCeldas()
+
+        GenerarFiltrosCustoms()
+    End Sub
+
+    Private Sub GenerarFiltrosCustoms()
+        'Display only custom filters created in Column1.
+        Dim fcd As New FarPoint.Win.Spread.FilterColumnDefinition(2, FarPoint.Win.Spread.FilterListBehavior.Custom)
+        Dim hf As New FarPoint.Win.Spread.HideRowFilter(FpSpread1.ActiveSheet)
+        hf.AddColumn(fcd)
+        FpSpread1.ActiveSheet.RowFilter = hf
+
+
+        Dim ccd As FarPoint.Win.Spread.FilterColumnDefinition = FpSpread1.ActiveSheet.RowFilter.GetFilterColumnDefinition(2)
+        'Add the custom filter created for Column1.
+        For i = 0 To Me.FpSpread1.ActiveSheet.Rows.Count - 1
+            Dim cfi As New CustomFilterButton(FpSpread1.ActiveSheet)
+            ccd.Filters.Add(cfi)
+
+        Next
+
+
+
+    End Sub
+
+    Private Sub GenerarCeldas()
+        FpSpread1.ActiveSheet.DefaultStyle.CellType = New FarPoint.Win.Spread.CellType.NumberCellType
+        FpSpread1.ActiveSheet.SetValue(0, 0, 10)
+        FpSpread1.ActiveSheet.SetValue(1, 0, 100)
+        FpSpread1.ActiveSheet.SetValue(2, 0, 50)
+        FpSpread1.ActiveSheet.SetValue(3, 0, 40)
+        FpSpread1.ActiveSheet.SetValue(4, 0, 80)
+        FpSpread1.ActiveSheet.SetValue(5, 0, 1)
+        FpSpread1.ActiveSheet.SetValue(6, 0, 65)
+        FpSpread1.ActiveSheet.SetValue(7, 0, 20)
+        FpSpread1.ActiveSheet.SetValue(8, 0, 30)
+        FpSpread1.ActiveSheet.SetValue(9, 0, 35)
+
+
+
+        For i = 0 To Me.FpSpread1.ActiveSheet.Rows.Count - 1
+
+            If i < 20 Then
+                Dim prctcell As New FarPoint.Win.Spread.CellType.ButtonCellType()
+                FpSpread1.ActiveSheet.Cells(i, 2).CellType = prctcell
+
+                prctcell.Text = "Text Button"
+
+                'Else
+
+                '    FpSpread1.ActiveSheet.Cells(i, 2).Text = "Hola"
+
+            End If
+
+
+
+        Next
+    End Sub
+
+
+End Class
