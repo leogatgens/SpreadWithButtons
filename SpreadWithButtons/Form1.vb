7f7b764308b0b2f91348269c83fb9b8531fd16e0
Leonardo Meléndez <leogatgens@gmail.com>
2018-02-23 15:39:00 -0600

    Ajustes para tomar el enter como evento

--- SpreadWithButtons/Form1.vb
+++ SpreadWithButtons/Form1.vb
@@ -1,7 +1,13 @@
-﻿Public Class form1
+﻿Imports FarPoint.Win.Spread
+Imports FarPoint.Win.Spread.CellType
+
+Public Class form1
 
 
     Dim WithEvents FpSpread1 As New FarPoint.Win.Spread.FpSpread()
+
+    Dim handler As EditorNotifyEventHandler
+
     Sub New()
 
         ' This call is required by the designer.
@@ -10,17 +16,56 @@
         ' Add any initialization after the InitializeComponent() call.
         FpSpread1 = New FarPoint.Win.Spread.FpSpread
         Dim hoja1 As New FarPoint.Win.Spread.SheetView
-        hoja1.RowCount = 20
+        hoja1.RowCount = 500
+        hoja1.ColumnCount = 70
 
         Dim hoja2 As New FarPoint.Win.Spread.SheetView
-        hoja2.RowCount = 20
+        hoja2.RowCount = 500
+        hoja2.ColumnCount = 70
         FpSpread1.Sheets.Add(hoja1)
         FpSpread1.Sheets.Add(hoja2)
 
         FpSpread1.Dock = DockStyle.Fill
         Me.Controls.Add(FpSpread1)
 
+
+        AddHandler FpSpread1.ButtonClicked, handler
+        AddHandler FpSpread1.KeyDown, AddressOf manejoEnter
+
+        'AddHandler FpSpread1.Enter, handler
+
+
+
+
+
+        Dim im As FarPoint.Win.Spread.InputMap = New FarPoint.Win.Spread.InputMap()
+
+        im = FpSpread1.GetInputMap(FarPoint.Win.Spread.InputMapMode.WhenFocused)
+
+
+
+        im.Put(New FarPoint.Win.Spread.Keystroke(Keys.Enter, Keys.Space), FarPoint.Win.Spread.SpreadActions.None)
+
+
+
+
+
+
+
+
     End Sub
+
+
+    Private Sub manejoEnter(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs)
+        If (e.KeyCode = Keys.[Return]) AndAlso (TypeOf FpSpread1.ActiveSheet.ActiveCell.CellType Is ButtonCellType) Then
+            FpSpread1_ButtonClicked(sender, New FarPoint.Win.Spread.EditorNotifyEventArgs(FpSpread1.GetRootWorkbook(), Nothing, FpSpread1.ActiveSheet.ActiveRowIndex, FpSpread1.ActiveSheet.ActiveColumnIndex))
+            FpSpread1.StopCellEditing()
+        End If
+    End Sub
+
+
+
+
     Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'Sheet1
         GenerateCells(0)
@@ -35,20 +80,51 @@
 
     Private Sub GenerateCells(index)
         For i = 0 To Me.FpSpread1.Sheets(index).Rows.Count - 1
-            Dim prctcell As New FarPoint.Win.Spread.CellType.ButtonCellType()
-            If i < 10 Then
-                prctcell.Text = "Text Button"
+            For j = 4 To Me.FpSpread1.Sheets(index).columns.Count - 1
+                Dim bttncell As New FarPoint.Win.Spread.CellType.ButtonCellType()
+                bttncell.BackgroundStyle = FarPoint.Win.BackStyle.Gradient
+                bttncell.ButtonColor = Color.Yellow
+                bttncell.ButtonColor2 = Color.Orange
+                bttncell.GradientMode = Drawing2D.LinearGradientMode.ForwardDiagonal
+                bttncell.ShadowSize = 20
 
-            Else
 
-                prctcell.Text = "Test"
 
-            End If
+                If i < 10 Then
+                    bttncell.Text = "Text Button"
+                    FpSpread1.Sheets(index).Cells(i, j).Value = bttncell.Text
 
-            FpSpread1.Sheets(index).Cells(i, 4).CellType = prctcell
-            'FpSpread1.Sheets(0).Cells(i, 5).CellType = prctcell
-            FpSpread1.Sheets(index).SetValue(i, 1, 69 + i)
+                Else
+
+                    bttncell.Text = "Test"
+                    FpSpread1.Sheets(index).Cells(i, j).Value = bttncell.Text
+
+                End If
+
+                FpSpread1.Sheets(index).Cells(i, j).CellType = bttncell
+                'FpSpread1.Sheets(0).Cells(i, 5).CellType = prctcell
+                'FpSpread1.Sheets(index).SetValue(i, 1, 69 + i)
+
+            Next
+
         Next
+    End Sub
+
+    Private Sub FpSpread1_ButtonClicked(ByVal sender As Object, ByVal e As FarPoint.Win.Spread.EditorNotifyEventArgs) Handles FpSpread1.ButtonClicked
+        'Dim algo = e.EditingControl
+        'algo.Text = "adfsssssssssssss"
+
+        Dim hoja = DirectCast(sender, FpSpread)
+
+        Dim prctcell As New FarPoint.Win.Spread.CellType.ButtonCellType()
+
+        Dim texto As String = "OJO"
+
+
+
+        FpSpread1.ActiveSheet.Cells(e.Row, e.Column).CellType = prctcell
+        prctcell.Text = texto
+        hoja.ActiveSheet.SetValue(e.Row, e.Column, texto)
     End Sub
 #Region "Sheet1"
 
@@ -100,31 +176,35 @@
 
         Dim indexSheet As Integer = 1
         ''Display only custom filters created in Column1.
-        Dim fcdBoton As New FarPoint.Win.Spread.FilterColumnDefinition(4, FarPoint.Win.Spread.FilterListBehavior.Custom)
+        Dim fcdBoton As New FarPoint.Win.Spread.FilterColumnDefinition(4, FarPoint.Win.Spread.FilterListBehavior.Default)
+        'Dim fcdBoton As New FarPoint.Win.Spread.FilterColumnDefinition(4, FarPoint.Win.Spread.FilterListBehavior.Custom)
 
 
-        Dim fcd As New FarPoint.Win.Spread.FilterColumnDefinition(1, FarPoint.Win.Spread.FilterListBehavior.SortByMostOccurrences Or FarPoint.Win.Spread.FilterListBehavior.Default)
-        Dim fcd1 As New FarPoint.Win.Spread.FilterColumnDefinition(2)
-        Dim fcd2 As New FarPoint.Win.Spread.FilterColumnDefinition
+        'Dim fcd As New FarPoint.Win.Spread.FilterColumnDefinition(1, FarPoint.Win.Spread.FilterListBehavior.SortByMostOccurrences Or FarPoint.Win.Spread.FilterListBehavior.Default)
+        'Dim fcd1 As New FarPoint.Win.Spread.FilterColumnDefinition(2)
+        'Dim fcd2 As New FarPoint.Win.Spread.FilterColumnDefinition
         Dim hf As New FarPoint.Win.Spread.HideRowFilter(FpSpread1.Sheets(indexSheet))
-        hf.AddColumn(fcd)
-        hf.AddColumn(fcd)
-        hf.AddColumn(fcd1)
+        'hf.AddColumn(fcd)
+        'hf.AddColumn(fcd)
+        'hf.AddColumn(fcd1)
         hf.AddColumn(fcdBoton)
 
         FpSpread1.Sheets(indexSheet).RowFilter = hf
-        FpSpread1.ActiveSheet.AutoFilterMode = FarPoint.Win.Spread.AutoFilterMode.FilterGadget
 
-        ''Add the custom filter created for Column1.
 
-        Dim ccd As FarPoint.Win.Spread.FilterColumnDefinition = FpSpread1.Sheets(indexSheet).RowFilter.GetFilterColumnDefinition(4)
-        Dim cfi As New CustomFilterButton(FpSpread1.Sheets(indexSheet), "Text Button")
-        Dim cfi2 As New CustomFilterButton(FpSpread1.Sheets(indexSheet), "Test")
 
-        ccd.Filters.Add(cfi)
-        ccd.Filters.Add(cfi2)
+#Disable Warning BC42303 ' XML comment cannot appear within a method or a property. XML comment will be ignored.
+        '''Add the custom filter created for Column1.
+
+        'Dim ccd As FarPoint.Win.Spread.FilterColumnDefinition = FpSpread1.Sheets(indexSheet).RowFilter.GetFilterColumnDefinition(4)
+        'Dim cfi As New CustomFilterButton(FpSpread1.Sheets(indexSheet), "Text Button")
+        'Dim cfi2 As New CustomFilterButton(FpSpread1.Sheets(indexSheet), "Test")
+
+        'ccd.Filters.Add(cfi)
+        'ccd.Filters.Add(cfi2)
 
         FpSpread1.Sheets(indexSheet).AutoFilterMode = FarPoint.Win.Spread.AutoFilterMode.EnhancedContextMenu
+#Enable Warning BC42303 ' XML comment cannot appear within a method or a property. XML comment will be ignored.
     End Sub
 #End Region
 
---
+++ SpreadWithButtons/Form2.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ButtonCellType9 As FarPoint.Win.Spread.CellType.ButtonCellType = New FarPoint.Win.Spread.CellType.ButtonCellType()
+        Dim ButtonCellType10 As FarPoint.Win.Spread.CellType.ButtonCellType = New FarPoint.Win.Spread.CellType.ButtonCellType()
+        Dim ButtonCellType11 As FarPoint.Win.Spread.CellType.ButtonCellType = New FarPoint.Win.Spread.CellType.ButtonCellType()
+        Dim ButtonCellType12 As FarPoint.Win.Spread.CellType.ButtonCellType = New FarPoint.Win.Spread.CellType.ButtonCellType()
+        Dim ButtonCellType13 As FarPoint.Win.Spread.CellType.ButtonCellType = New FarPoint.Win.Spread.CellType.ButtonCellType()
+        Dim ButtonCellType14 As FarPoint.Win.Spread.CellType.ButtonCellType = New FarPoint.Win.Spread.CellType.ButtonCellType()
+        Dim ButtonCellType15 As FarPoint.Win.Spread.CellType.ButtonCellType = New FarPoint.Win.Spread.CellType.ButtonCellType()
+        Dim ButtonCellType16 As FarPoint.Win.Spread.CellType.ButtonCellType = New FarPoint.Win.Spread.CellType.ButtonCellType()
+        Me.FpSpread1 = New FarPoint.Win.Spread.FpSpread()
+        Me.FpSpread1_Sheet1 = New FarPoint.Win.Spread.SheetView()
+        CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FpSpread1_Sheet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'FpSpread1
+        '
+        Me.FpSpread1.AccessibleDescription = "FpSpread1, Sheet1, Row 0, Column 0, "
+        Me.FpSpread1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FpSpread1.Location = New System.Drawing.Point(0, 0)
+        Me.FpSpread1.Name = "FpSpread1"
+        Me.FpSpread1.Sheets.AddRange(New FarPoint.Win.Spread.SheetView() {Me.FpSpread1_Sheet1})
+        Me.FpSpread1.Size = New System.Drawing.Size(747, 396)
+        Me.FpSpread1.TabIndex = 0
+        '
+        'FpSpread1_Sheet1
+        '
+        Me.FpSpread1_Sheet1.Reset()
+        Me.FpSpread1_Sheet1.SheetName = "Sheet1"
+        'Formulas and custom names must be loaded with R1C1 reference style
+        Me.FpSpread1_Sheet1.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.R1C1
+        Me.FpSpread1_Sheet1.Cells.Get(6, 4).CellPadding.Bottom = 1
+        Me.FpSpread1_Sheet1.Cells.Get(6, 4).CellPadding.Left = 1
+        Me.FpSpread1_Sheet1.Cells.Get(6, 4).CellPadding.Right = 1
+        Me.FpSpread1_Sheet1.Cells.Get(6, 4).CellPadding.Top = 1
+        ButtonCellType9.ButtonColor2 = System.Drawing.SystemColors.ButtonFace
+        Me.FpSpread1_Sheet1.Cells.Get(6, 4).CellType = ButtonCellType9
+        Me.FpSpread1_Sheet1.Cells.Get(6, 10).NoteStyle = FarPoint.Win.Spread.NoteStyle.StickyNote
+        ButtonCellType10.ButtonColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer))
+        ButtonCellType10.ButtonColor2 = System.Drawing.Color.Blue
+        ButtonCellType10.DarkColor = System.Drawing.Color.Gray
+        ButtonCellType10.LightColor = System.Drawing.Color.Yellow
+        ButtonCellType10.Text = "Prueba"
+        Me.FpSpread1_Sheet1.Columns.Get(0).CellType = ButtonCellType10
+        ButtonCellType11.ButtonColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer))
+        ButtonCellType11.ButtonColor2 = System.Drawing.Color.Blue
+        ButtonCellType11.DarkColor = System.Drawing.Color.Gray
+        ButtonCellType11.LightColor = System.Drawing.Color.Yellow
+        ButtonCellType11.Text = "Prueba"
+        Me.FpSpread1_Sheet1.Columns.Get(1).CellType = ButtonCellType11
+        ButtonCellType12.ButtonColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer))
+        ButtonCellType12.ButtonColor2 = System.Drawing.Color.Blue
+        ButtonCellType12.DarkColor = System.Drawing.Color.Gray
+        ButtonCellType12.LightColor = System.Drawing.Color.Yellow
+        ButtonCellType12.Text = "Prueba"
+        Me.FpSpread1_Sheet1.Columns.Get(2).CellType = ButtonCellType12
+        ButtonCellType13.ButtonColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer))
+        ButtonCellType13.ButtonColor2 = System.Drawing.Color.Blue
+        ButtonCellType13.DarkColor = System.Drawing.Color.Gray
+        ButtonCellType13.LightColor = System.Drawing.Color.Yellow
+        ButtonCellType13.Text = "Prueba"
+        Me.FpSpread1_Sheet1.Columns.Get(3).CellType = ButtonCellType13
+        ButtonCellType14.ButtonColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer))
+        ButtonCellType14.ButtonColor2 = System.Drawing.Color.Blue
+        ButtonCellType14.DarkColor = System.Drawing.Color.Gray
+        ButtonCellType14.LightColor = System.Drawing.Color.Yellow
+        ButtonCellType14.Text = "Prueba"
+        Me.FpSpread1_Sheet1.Columns.Get(4).CellType = ButtonCellType14
+        ButtonCellType15.ButtonColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer))
+        ButtonCellType15.ButtonColor2 = System.Drawing.Color.Blue
+        ButtonCellType15.DarkColor = System.Drawing.Color.Gray
+        ButtonCellType15.LightColor = System.Drawing.Color.Yellow
+        ButtonCellType15.Text = "Prueba"
+        Me.FpSpread1_Sheet1.Columns.Get(5).CellType = ButtonCellType15
+        ButtonCellType16.ButtonColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer))
+        ButtonCellType16.ButtonColor2 = System.Drawing.Color.Blue
+        ButtonCellType16.DarkColor = System.Drawing.Color.Gray
+        ButtonCellType16.LightColor = System.Drawing.Color.Yellow
+        ButtonCellType16.Text = "Prueba"
+        Me.FpSpread1_Sheet1.Columns.Get(6).CellType = ButtonCellType16
+        Me.FpSpread1_Sheet1.RowHeader.Columns.Default.Resizable = False
+        Me.FpSpread1_Sheet1.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.A1
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(747, 396)
+        Me.Controls.Add(Me.FpSpread1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FpSpread1_Sheet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FpSpread1 As FarPoint.Win.Spread.FpSpread
+    Friend WithEvents FpSpread1_Sheet1 As FarPoint.Win.Spread.SheetView
+End Class
