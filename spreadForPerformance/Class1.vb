4d16741f3c2bb8b2520b3c69ef14e8e564b4711c
Leonardo Meléndez <leogatgens@gmail.com>
2018-06-01 16:47:07 -0600

    ajuste min9imo

--- spreadForPerformance/Class1.vb
+++ spreadForPerformance/Class1.vb
@@ -22,4 +22,36 @@ Public Class Class1
         End While
     End Sub
 
+
+    Private Sub RecorrerErroresDeHoja(ByVal sheet As SheetView)
+
+        Dim instance As DefaultSheetDataModel = CType(sheet.Models.Data, DefaultSheetDataModel)
+        Dim value As Integer
+
+        value = instance.GetNonEmptyErrorTextsColumnCount()
+
+        Dim enumerator As DefaultSheetDataModel.DefaultSheetDataModelEnumerator = (CType(sheet.Models.Data, DefaultSheetDataModel)).EnumFormulas
+
+        Dim indiceInicial As Integer = enumerator.NextNonEmptyRow(-1)
+
+        While indiceInicial <> -1
+            Dim proximoIndiceColumna As Integer = enumerator.NextNonEmptyColumnInRow(indiceInicial, -1)
+
+            While proximoIndiceColumna <> -1
+                Dim expr = sheet.GetValue(indiceInicial, proximoIndiceColumna)
+                If TypeOf expr Is CalcError Then
+                    Dim algo = expr.ToString
+
+
+                End If
+
+
+
+                proximoIndiceColumna = enumerator.NextNonEmptyColumnInRow(indiceInicial, proximoIndiceColumna)
+            End While
+
+            indiceInicial = enumerator.NextNonEmptyRow(indiceInicial)
+        End While
+    End Sub
+
 End Class
