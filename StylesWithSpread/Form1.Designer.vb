8e7d7fa9f2e187927850fc4673bc8f470d382dd2
Leonardo Meléndez <leogatgens@gmail.com>
2018-09-20 16:25:31 -0600

    Pruebas para eliminar formulas

---
+++ StylesWithSpread/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FpSpread1 = New FarPoint.Win.Spread.FpSpread()
+        Me.FpSpread1_Sheet1 = New FarPoint.Win.Spread.SheetView()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.QuitarFormulasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FormulaTextBox1 = New FarPoint.Win.Spread.FormulaTextBox()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FpSpread1_Sheet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.FormulaTextBox1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.MenuStrip1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.FpSpread1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(736, 512)
+        Me.SplitContainer1.SplitterDistance = 57
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AbrirToolStripMenuItem, Me.QuitarFormulasToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(736, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(45, 20)
+        Me.AbrirToolStripMenuItem.Text = "Abrir"
+        '
+        'FpSpread1
+        '
+        Me.FpSpread1.AccessibleDescription = ""
+        Me.FpSpread1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FpSpread1.Location = New System.Drawing.Point(0, 0)
+        Me.FpSpread1.Name = "FpSpread1"
+        Me.FpSpread1.Sheets.AddRange(New FarPoint.Win.Spread.SheetView() {Me.FpSpread1_Sheet1})
+        Me.FpSpread1.Size = New System.Drawing.Size(736, 451)
+        Me.FpSpread1.TabIndex = 0
+        '
+        'FpSpread1_Sheet1
+        '
+        Me.FpSpread1_Sheet1.Reset()
+        Me.FpSpread1_Sheet1.SheetName = "Sheet1"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'QuitarFormulasToolStripMenuItem
+        '
+        Me.QuitarFormulasToolStripMenuItem.Name = "QuitarFormulasToolStripMenuItem"
+        Me.QuitarFormulasToolStripMenuItem.Size = New System.Drawing.Size(102, 20)
+        Me.QuitarFormulasToolStripMenuItem.Text = "Quitar formulas"
+        '
+        'FormulaTextBox1
+        '
+        Me.FormulaTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormulaTextBox1.Location = New System.Drawing.Point(0, 24)
+        Me.FormulaTextBox1.Multiline = False
+        Me.FormulaTextBox1.Name = "FormulaTextBox1"
+        Me.FormulaTextBox1.Size = New System.Drawing.Size(736, 33)
+        Me.FormulaTextBox1.TabIndex = 1
+        'Attach FormulaTextBox1 to FpSpread1
+        Me.FormulaTextBox1.Attach(Me.FpSpread1)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(736, 512)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FpSpread1_Sheet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AbrirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FpSpread1 As FarPoint.Win.Spread.FpSpread
+    Friend WithEvents FpSpread1_Sheet1 As FarPoint.Win.Spread.SheetView
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents QuitarFormulasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormulaTextBox1 As FarPoint.Win.Spread.FormulaTextBox
+End Class
---
+++ StylesWithSpread/Form1.vb
@@ -0,0 +1,75 @@
+﻿Imports FarPoint.Win.Spread
+Imports FarPoint.Win.Spread.Model
+
+Public Class Form1
+
+    Dim tiempo As New TimeSpan
+    Dim HoraFin As New TimeSpan
+    Private Sub AbrirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AbrirToolStripMenuItem.Click
+        Dim fileName As String = Nothing
+
+        Using openFileDialog1 As OpenFileDialog = New OpenFileDialog()
+            openFileDialog1.InitialDirectory = "c:\"
+            openFileDialog1.Filter = "Excel Files(.xls)|*.xls|Excel Files(.xlsx)|*.xlsx| Excel Files(*.xlsm)|*.xlsm"
+            openFileDialog1.FilterIndex = 2
+            openFileDialog1.RestoreDirectory = True
+
+            If openFileDialog1.ShowDialog() = DialogResult.OK Then
+                fileName = openFileDialog1.FileName
+            End If
+        End Using
+
+        If fileName IsNot Nothing Then
+
+            RegistroInicioProceso()
+            FpSpread1.OpenExcel(fileName, FarPoint.Excel.ExcelOpenFlags.TruncateEmptyRowsAndColumns)
+            'FpSpread1.OpenExcel(fileName)
+
+            RegistrarFinProceso()
+        End If
+    End Sub
+
+    Private Sub RegistrarFinProceso()
+        HoraFin = DateTime.Now.TimeOfDay
+
+        Dim duracion = HoraFin - tiempo
+        Debug.WriteLine("tiempo: " & duracion.ToString)
+    End Sub
+
+    Private Sub RegistroInicioProceso()
+        tiempo = DateTime.Now.TimeOfDay
+    End Sub
+
+    Private Sub QuitarFormulasToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles QuitarFormulasToolStripMenuItem.Click
+
+        EliminaLasFormulasDeLaHojaModelo()
+
+
+        ''Copiar modelo
+        ''Copiar estilos
+    End Sub
+
+    Public Sub EliminaLasFormulasDeLaHojaModelo()
+        Dim archivo As FpSpread = FpSpread1
+        If archivo IsNot Nothing Then
+
+
+            RegistroInicioProceso()
+
+            archivo.Sheets(1).SelectionPolicy = SelectionPolicy.MultiRange
+            archivo.Sheets(1).AddSelection(0, 0, archivo.Sheets(1).NonEmptyRowCount,
+                                           archivo.Sheets(1).NonEmptyColumnCount)
+            archivo.Sheets(1).ClipboardCut(ClipboardCopyOptions.Formulas)
+            Clipboard.Clear()
+
+            'Dim archivoTemporal As New FpSpread
+            'archivoTemporal.Sheets.Add(New SheetView With {.SheetName = "HojaCopia"})
+            'archivoTemporal.Sheets(0).ClipboardPaste(ClipboardPasteOptions.Formulas)
+            'archivoTemporal.Sheets.Clear()
+
+            RegistrarFinProceso()
+        End If
+
+
+    End Sub
+End Class
