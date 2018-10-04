6fc593df28bc72028af5c21aa74a54605baa261a
Leonardo Meléndez <leogatgens@gmail.com>
2018-10-04 11:48:16 -0600

    Ajustes para exportar

---
+++ StylesWithSpread/Form2.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ContosoUniversity2DataSet = New StylesWithSpread.ContosoUniversity2DataSet()
+        Me.ContosoUniversity2DataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ContosoUniversity2DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ContosoUniversity2DataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.DataSource = Me.ContosoUniversity2DataSetBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(55, 69)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'ContosoUniversity2DataSet
+        '
+        Me.ContosoUniversity2DataSet.DataSetName = "ContosoUniversity2DataSet"
+        Me.ContosoUniversity2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ContosoUniversity2DataSetBindingSource
+        '
+        Me.ContosoUniversity2DataSetBindingSource.DataSource = Me.ContosoUniversity2DataSet
+        Me.ContosoUniversity2DataSetBindingSource.Position = 0
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ContosoUniversity2DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ContosoUniversity2DataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ContosoUniversity2DataSetBindingSource As BindingSource
+    Friend WithEvents ContosoUniversity2DataSet As ContosoUniversity2DataSet
+End Class
